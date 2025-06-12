9fccafe3f3433baf56702d467dcba9e47f9e2a21
Cosarca Maxim <128998700+Mathuska@users.noreply.github.com>
2025-06-12 18:11:25 +0300

    revert last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import {Routes, Route} from "react-router"
-import router from "./router"
-import NavBarLayout from "./layouts/NavBarLayout"
+import { Routes, Route } from "react-router";
+import router from "./router";
+import NavBarLayout from "./layouts/NavBarLayout";
 
 export default function App() {
   return (
@@ -8,9 +8,14 @@ export default function App() {
       {router.map((route, index) => {
         return (
           <NavBarLayout showNavBar={true}>
-            <Route key={index} path={route.path} element={route.element}></Route>
+            <Route
+              key={index}
+              path={route.path}
+              element={route.element}
+            ></Route>
           </NavBarLayout>
-      )})}
+        );
+      })}
     </Routes>
-  )
+  );
 }
