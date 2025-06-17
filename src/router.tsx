14fe28d2d21e3bc2daa327889e7cae3a0c269210
Cosarca Maxim <128998700+Mathuska@users.noreply.github.com>
2025-06-17 19:32:30 +0300

    fix rebase issues

--- src/router.tsx
+++ src/router.tsx
@@ -1,44 +1,8 @@
-<<<<<<< HEAD
-
-import Home from "./pages/Home"
-import Cart from "./pages/Cart"
-import Liked from "./pages/Liked"
-import User from "./pages/User"
-import AllProducts from "./pages/AllProducts"
-
-
-const router = [
-    {
-        path: "/",
-        element: <Home />,
-        showNavBar: true
-    }, 
-    {
-        path: "/cart",
-        element: <Cart />,
-        showNavBar: true
-    }, 
-    {
-        path: "/liked",
-        element: <Liked />,
-        showNavBar: true
-    }, 
-    {
-        path: "/user",
-        element: <User />,
-        showNavBar: true
-    },
-    {
-        path: "/products",
-        element: <AllProducts />,
-        showNavBar: true
-    }
-]
-=======
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Liked from "./pages/Liked";
 import User from "./pages/User";
+import AllProducts from "./pages/AllProducts";
 
 const router = [
   {
@@ -61,7 +25,11 @@ const router = [
     element: <User />,
     showNavBar: true,
   },
+  {
+    path: "/products",
+    element: <AllProducts />,
+    showNavBar: true,
+  },
 ];
->>>>>>> 15c50fe (WIP call-to-action-card)
 
 export default router;
