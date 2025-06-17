050732088a3747c0212571361aefd0453db3ddd7
Olga <olycaraman@gmail.com>
2025-06-17 19:19:34 +0300

    add liked page

--- src/pages/Liked.tsx
+++ src/pages/Liked.tsx
@@ -1,7 +1,17 @@
+import {StarHalf} from "lucide-react";
+import Footer from "../components/core/Footer.tsx";
 
 const Liked = () =>{
     return (
-        <div className="page-container">Liked</div>
+        <>
+            <div className="w-screen h-10  px-4 py-2 flex justify-center bg-[#E4EBED]">
+                <StarHalf className="fill-yellow-500 stroke-yellow-500"/>Save items<StarHalf className=" rotate-y-180 fill-yellow-500 stroke-yellow-500"/>
+            </div>
+            <div className="page-container w-screen h-auto px-4 py-2 flex flex-wrap gap-4 ">
+            </div>
+            <Footer />
+        </>
+
     )
 }
 export default Liked
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+import colors from "./src/components/core/Colors.js";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
     },
-
   },
-  plugins: [],
+  plugins: [
+
+
+
+  ],
 };
