e839752d13f56ff0369f75b4e0891c9025f88e4c
nichita <nichita.tonu@orizont.md>
2025-06-17 18:59:03 +0300

    implemented listening box on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-
+import Listening from "../components/core/Listening";
 
 const Home = () =>{
     return (
         <div className="page-container">
             Home
+            <Listening/>
         </div>
     )
 
