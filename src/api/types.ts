1fd6c0c4b05b7fc8f21da9f363ded4a175bda0e7
Olga <olycaraman@gmail.com>
2025-06-19 21:07:38 +0300

    add axios interceptors

---
+++ src/api/types.ts
@@ -0,0 +1,42 @@
+// @ts-ignore
+enum UserRoleEnum {
+    ADMIN = "ADMIN",
+    USER = "USER"
+}
+
+export interface RegisterData {
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string,
+    phoneNumber: string
+}
+
+export interface RegisterResponse {
+    accessToken: string,
+    refreshToken: string
+}
+
+export interface LoginData {
+    email: string,
+    password: string,
+}
+
+export interface LoginResponse {
+    accessToken: string,
+    refreshToken: string
+}
+
+export interface MySelfResponse {
+    id: string,
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt: Date,
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string,
+    phoneNumber: string,
+    role: UserRoleEnum
+}
+
